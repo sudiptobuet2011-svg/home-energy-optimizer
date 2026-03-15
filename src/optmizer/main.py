1c6be3c56982f4d26d63a8dfeab71d2aa9c32f73
Sudipto Bosu <sudiptobuez2011@gmail.com>
2026-03-16 00:15:53 +0100

    test commit

--- src/optmizer/main.py
+++ src/optmizer/main.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-prices = pd.read_csv(
-    "data/raw/day_ahead_prices.csv",
-    sep=";",
-    decimal=","
-)
+prices = pd.read_csv("data/raw/day_ahead_prices.csv", sep=";", decimal=",")
+pv = pd.read_csv("data/raw/pv_generation.csv", sep=";", decimal=",")
+load = pd.read_csv("data/raw/consumption_profile.csv", sep=";", decimal=",")
 
-prices = prices.rename(columns={
-    "HourUTC": "time",
-    "SpotPriceEUR": "price"
-})
+prices = prices.rename(columns={"HourUTC": "time", "SpotPriceEUR": "price"})
+pv = pv.rename(columns={"Zeit": "time", "PV": "pv_generation"})
+load = load.rename(columns={"Zeit": "time", "Verbrauch": "base_load"})
 
-print(prices.head())
+prices["time"] = pd.to_datetime(prices["time"])
+pv["time"] = pd.to_datetime(pv["time"])
+load["time"] = pd.to_datetime(load["time"])
+
+df = prices.merge(pv, on="time").merge(load, on="time")
+
+df = df.set_index("time")
+
+print(df.head())
